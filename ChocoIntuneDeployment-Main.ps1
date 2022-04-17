19b9123075ab1cfa5eae5620ff3cfaeec0b79e47
Lucas <lucas.edinger@stud.hn.de>
2022-04-17 18:49:27 +0200

    Update ChocoIntuneDeployment-Main.ps1
    Added Some Error Handling

--- ChocoIntuneDeployment-Main.ps1
+++ ChocoIntuneDeployment-Main.ps1
@@ -1,4 +1,4 @@
-﻿##Before you can use, you need to Connect to Intune API to the right tenant with following Command:
+##Before you can use, you need to Connect to Intune API to the right tenant with following Command:
 #Connect-MSIntuneGraph -TenantID <tenant-ID>
 #Sample : Connect-MSIntuneGraph -TenantID "contoso.com"
 #If you using the first time, you need to run Install-IntunechocoComponents with elevated rights to install the required Components
@@ -32,32 +32,34 @@ $WorkPath = $env:TEMP
 if($ChocoName -eq $Null){return "No software selected, repeat it"}
 
 ### Get Informations of Software
+        Write-Host "Getting Infos from Chocolatey"
         $Infos = Get-ChocoInfo $ChocoName
 ###
 
-$AppName = $Infos.Title.Split("|")[0]
+        $AppName = $Infos.Title.Split("|")[0]
 
-$intuneApps = Get-IntuneWin32App
-foreach($IntuneAPP in $intuneApps)
-{
-if (($IntuneAPP.displayName).ToLower() -eq $AppName.ToLower()){write-host "app is already deployed, please update or remove first !";return $Null}
-}
+        $intuneApps = Get-IntuneWin32App
+        foreach($IntuneAPP in $intuneApps)
+        {
+            if (($IntuneAPP.displayName).ToLower() -eq $AppName.ToLower()){write-host "app is already deployed, please update or remove first !";return $Null}
+        }
 
 ### Icon Creation
-
-$Icon = Get-ChocoIconBase64 -ChocoName $ChocoName
+        write-host "Getting Icon from chocolatey library"
+        $Icon = Get-ChocoIconBase64 -ChocoName $ChocoName
         
 ###
 
 ###Install And Detection Script Creation
-        New-Item -Path $WorkPath -Name "intunedeployment" -ItemType Directory -Force
-        New-Item -Path ($WorkPath+"\intunedeployment") -Name "source" -ItemType Directory -Force
+        write-host "Creating the install and detection script in Temp Folder"
+        try{$Output = New-Item -Path $WorkPath -Name "intunedeployment" -ItemType Directory -Force}catch{"Some Error 55"}
+        try{$Output = New-Item -Path ($WorkPath+"\intunedeployment") -Name "source" -ItemType Directory -Force}catch{"Some Error 56"}
 
         $WorkPath =  ($WorkPath+"\intunedeployment")
         $SourcePath = ($WorkPath + "\source")
 
-        New-Item ($SourcePath + "\install.ps1") -Force
-        Set-Content ($SourcePath + "\install.ps1") ('
+        try{$Output = New-Item ($SourcePath + "\install.ps1") -Force}catch{"Some Error 61"}
+        try{$Output = Set-Content ($SourcePath + "\install.ps1") ('
         
         param(
             [string]$package = "",
@@ -83,9 +85,9 @@ $Icon = Get-ChocoIconBase64 -ChocoName $ChocoName
         $task = New-ScheduledTask –Action $taskAction –Principal $taskUserPrincipal –Trigger $taskTrigger –Settings $taskSettings
         Register-ScheduledTask –TaskName ("Update " + $ChocoName + " on startup")  –InputObject $task -TaskPath "Intune-Choco-Updates" –Force
 
-        ')
+        ')}catch{"Some Error 62"}
 
-        New-Item ($SourcePath + "\detection.ps1") -Force
+        try{$Output = New-Item ($SourcePath + "\detection.ps1") -Force}catch{"Some Error 90"}
         
         
         $DetectionContent = ('
@@ -96,15 +98,15 @@ $Icon = Get-ChocoIconBase64 -ChocoName $ChocoName
         exit $LastExitCode
         ')
 
-        Set-Content  ($Sourcepath + "\detection.ps1") $DetectionContent
+        try{Set-Content  ($Sourcepath + "\detection.ps1") $DetectionContent}catch{"Some Error 101"}
 
         try{Remove-Item ($WorkPath + "\install.intunewin")}catch{}
 
-        $IntuneWinFile = New-IntuneWin32AppPackage -SourceFolder $SourcePath -SetupFile "install.ps1" -OutputFolder $WorkPath
+        try{$IntuneWinFile = New-IntuneWin32AppPackage -SourceFolder $SourcePath -SetupFile "install.ps1" -OutputFolder $WorkPath}catch{"Some Error 105"}
 ###
 
 ### Detection Script
-        $DetectionScript =  New-IntuneWin32AppDetectionRule -PowerShellScript -ScriptFile ($Workpath + "\source\detection.ps1")
+        try{$DetectionScript =  New-IntuneWin32AppDetectionRule -PowerShellScript -ScriptFile ($Workpath + "\source\detection.ps1")}catch{"Some Error 109"}
 
 ###
 
@@ -113,11 +115,11 @@ $Icon = Get-ChocoIconBase64 -ChocoName $ChocoName
 
 
 ### Create custom requirement rule
-        $RequirementRule = New-IntuneWin32AppRequirementRule -Architecture All -MinimumSupportedOperatingSystem 1607
+        try{$RequirementRule = New-IntuneWin32AppRequirementRule -Architecture All -MinimumSupportedOperatingSystem 1607}catch{"Some Error 118"}
 ###
 
 ### Chocolatey Dependency
-
+        write-host "Checking if Chocolatey is installed"
         $ChocoID = get-ChocoWin32ID
 
         $Dependency = New-IntuneWin32AppDependency -ID $ChocoID -DependencyType AutoInstall
@@ -126,17 +128,31 @@ $Icon = Get-ChocoIconBase64 -ChocoName $ChocoName
 
 ### Deploy Application
         $DisplayName = $ChocoName
-        $IntuneApp = Add-IntuneWin32App  -FilePath $IntuneWinFile.Path -DisplayName $AppName -Description $Infos.Description -Notes "PoweredByChocolatey" -Publisher "-" -InformationURL $Infos.SoftwareSite -PrivacyURL $Infos.SoftwareLicense -InstallExperience system -RestartBehavior suppress -DetectionRule $DetectionScript -RequirementRule $RequirementRule -InstallCommandLine $InstallCommandLine -UninstallCommandLine $UninstallCommandLine -Icon $Icon -Verbose
-        Add-IntuneWin32AppDependency -ID $IntuneApp.ID -Dependency $Dependency
-        write-host $Intent
+        Write-Host ("Creating and Uploading " + $ChocoName + " as win32 App to Intune")
+        try{$IntuneApp = Add-IntuneWin32App  -FilePath $IntuneWinFile.Path -DisplayName $AppName -Description $Infos.Description -Notes "PoweredByChocolatey" -Publisher "-" -InformationURL $Infos.SoftwareSite -PrivacyURL $Infos.SoftwareLicense -InstallExperience system -RestartBehavior suppress -DetectionRule $DetectionScript -RequirementRule $RequirementRule -InstallCommandLine $InstallCommandLine -UninstallCommandLine $UninstallCommandLine -Icon $Icon }catch{"Some Error 131"}
+        Write-Host "Adding dependency for chocolatey to the created Application"
+        try{Add-IntuneWin32AppDependency -ID $IntuneApp.ID -Dependency $Dependency}catch{"Some Error 132"}
         if($Required -eq $true){
-        Add-IntuneWin32AppAssignmentAllUsers -ID $intuneApp.id -Intent required -Notification showReboot -Verbose}
+            write-host "Enable autodeployment for all users"
+            try{
+            Add-IntuneWin32AppAssignmentAllUsers -ID $intuneApp.id -Intent required -Notification showReboot 
+            }
+            catch{"Some Error 135"}
+        }
         if($Required -eq $false){
-        Add-IntuneWin32AppAssignmentAllUsers -ID $intuneApp.id -Intent available -Notification showReboot -Verbose}
+            write-host "Making application available in companyportal for all users"
+            try{$Output = Add-IntuneWin32AppAssignmentAllUsers -ID $intuneApp.id -Intent available -Notification showReboot }
+            catch{"Some Error 137 "+$Output}
+        }
         
 ###
+        if($IntuneApp.id -ne $Null){write-host "Deoloyment was Successfull!"
+        return $intuneApp.id}
+        else{
+        return "Some Error uccured, please check and try again"
+        }
+        
 
-        return $intuneApp
 
 }#Main function for deploying The Applications
 
@@ -173,6 +189,7 @@ return $returnObject
 
 function install-chocolatey{
 
+write-host "Starting Deploy Chocolatey as win32 app to intune"
 $WorkPath = $ENV:temp
 
 $Icon = "iVBORw0KGgoAAAANSUhEUgAAAOwAAADVCAMAAABjeOxDAAAAw1BMVEWAteP///+SRxaaxOlxmMpwl8mfUyZ5OBJ/uuuLu+WUwed5qNh2sOGTPQBmkcfq6uucTRu8jXaTQgC7oJNzKwCSRAp1odGBrdWBPRPx9fqtzuyEn72Glq+Mc3KLeX2IipuJhJGRTCGKf4jH3fJ5MwCDpMbR4/SNbGbo8fqPW0W1xtm41O6OYlKQVDaNaF+RUS2Otdlzf5+hSw
@@ -197,33 +214,41 @@ LQQWmH6uHVbReIn7s/r0c+Ww+nYeXVx9GWaAHaisIqw2/Vw3LLDfI8a1Ov1cNayWnRRY2iWTzRruWZ21
 XX05KPF2i27Y8+CGu9uuFj7yKtK/4fYGNrBcHaLmCXdHMnNyIttBkNwFoORAy6Z2X1gRVpoc3oFx0W2rQ8wvZ752X1gu0dHwdlBvcaLPDUETY5M6on7JHWQUDSsmxZfjw7qyfsgRaEPU4/e1jwXrNBE6mJBFvSgrDHrQHrtWbnTk002D0tDHuYfmxX6/cbYfWHLZIyDHuoLCxqmX4jqCTYjBaBLRe2DJt3ctjzp+EKsD31jIBgAizyffTvfr4yu0O0
 evuFLfY7AkEweLyX7S9XYKsOA3mLyoJB804QdFbg+a5rsOeXfqAD5wtbps07GWnTH7ii3T92VN68smDyvBOw4bWT7m01ZBJwNv0woR0TBLdCWtjz4+DIm1UWh0NMt+NTyQQHZ7D9zKVsiB4Uv3rbZ6x0YctWQdB/uV3SwtKMlXr0ng1vnnRvq+EQ1B7cqv0Pc9wIDT8ePVAAAAAASUVORK5CYII="
 
-new-item -Path $WorkPath -Name "ChocoDeploy" -ItemType Directory -Force
+write-host "Creating install Script"
+try{new-item -Path $WorkPath -Name "ChocoDeploy" -ItemType Directory -Force}catch{"Some Error 216"}
 $WorkPath = $WorkPath + "\ChocoDeploy"
 
-new-item -Path $WorkPath -Name "Source" -ItemType Directory -Force
+try{new-item -Path $WorkPath -Name "Source" -ItemType Directory -Force}catch{"Some Error 219"}
 
 $SourcePath = $WorkPath + "\Source"
 
-New-Item $SourcePath -Name "install.ps1" -ItemType File -Force
+try{New-Item $SourcePath -Name "install.ps1" -ItemType File -Force}catch{"Some Error 223"}
 
 $InstallScriptContent = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
-$RequirementRule = New-IntuneWin32AppRequirementRule -Architecture All -MinimumSupportedOperatingSystem 1607
+try{$RequirementRule = New-IntuneWin32AppRequirementRule -Architecture All -MinimumSupportedOperatingSystem 1607}catch{"Some Error 227"}
 
-Set-Content ($SourcePath + "\install.ps1") $InstallScriptContent
+try{Set-Content ($SourcePath + "\install.ps1") $InstallScriptContent}catch{"Some Error 229"}
 
-try{Remove-Item ($WorkPath + "\install.intunewin")}catch{}
-$AppPackage = New-IntuneWin32AppPackage -SourceFolder $SourcePath -SetupFile "install.ps1" -OutputFolder $WorkPath
- 
-$DetectionRule = New-IntuneWin32AppDetectionRuleFile -Existence -DetectionType exists -Path "C:\ProgramData\chocolatey" -FileOrFolder "choco.exe"
+try{Remove-Item ($WorkPath + "\install.intunewin")}catch{"Some Error 231"}
 
+Write-Host "Creating Win32 APP Package File"
+try{$AppPackage = New-IntuneWin32AppPackage -SourceFolder $SourcePath -SetupFile "install.ps1" -OutputFolder $WorkPath}catch{"Some Error 233"}
+write-host "Creating Detection Rule" 
+try{$DetectionRule = New-IntuneWin32AppDetectionRuleFile -Existence -DetectionType exists -Path "C:\ProgramData\chocolatey" -FileOrFolder "choco.exe"}catch{"Some Error 235"}
 
+write-host "Starting uploading to intune"
+try{$APP = Add-IntuneWin32App -FilePath $AppPackage.Path -DisplayName "Chocolatey" -RequirementRule $RequirementRule -Notes "FiredWithPowershell" -Description "Chocolatey Package Manager" -Publisher "www.linkedin.com/in/LuEd" -RestartBehavior suppress -InstallExperience system  -Icon $Icon -InstallCommandLine "powershell.exe -executionpolicy bypass .\install.ps1" -UninstallCommandLine "powershell.exe -executionpolicy bypass .\uninstall.ps1" -DetectionRule $DetectionRule}catch{"Some Error 237"}
 
-$APP = Add-IntuneWin32App -FilePath $AppPackage.Path -DisplayName "Chocolatey" -RequirementRule $RequirementRule -Notes "FiredWithPowershell" -Description "Chocolatey Package Manager" -Publisher "www.linkedin.com/in/LuEd" -RestartBehavior suppress -InstallExperience system  -Icon $Icon -InstallCommandLine "powershell.exe -executionpolicy bypass .\install.ps1" -UninstallCommandLine "powershell.exe -executionpolicy bypass .\uninstall.ps1" -DetectionRule $DetectionRule 
-
-Add-IntuneWin32AppAssignmentAllUsers -ID $App.id -Intent available -Notification hideAll -Verbose
-
-return $APP
+write-host "Make application installable for everyone in the company"
+try{Add-IntuneWin32AppAssignmentAllUsers -ID $App.id -Intent available -Notification hideAll }catch{"Some Error 239"}
+if ($APP.id -ne $Null){
+write-host "chocolatey successfully installed."
+return $APP.id
+}
+else{
+return "Some Error Occured"
+}
 }# Deploys Chocolatey to Intune-Tenant
 
 function get-ChocoWin32ID {
@@ -233,10 +258,10 @@ while($true){
 $APP = Get-IntuneWin32App -DisplayName "chocolatey"
 
 if ($APP.notes -like "*FiredWithPowershell*"){
-
+write-host "chocolatey found "
 return $APP.id}
 else{
-
+write-host "Chocolatey not found in intune, it will be deployed now"
 $Install = install-chocolatey
 
 }}
@@ -269,7 +294,8 @@ inkscape --export-type="png" $FilePath
 
 $FilePath = ($ENV:Temp +"\"+ $ChocoName + ".png")
 
-write-host "starting converting, waiting 5 Seconds"
+write-host "Icon file is in .svg format it will be converted to PNG"
+write-host "starting converting Image, waiting 5 Seconds"
 Start-Sleep -s 5
 
 
